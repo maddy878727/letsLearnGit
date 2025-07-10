a62c8f1b354c1712326034ced4c6968f4a298f98
madhuri yadav <madhyadav3592@gmail.com>
2025-07-10 22:58:53 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+
+    Run|Debug
+    public static void main(string[]args);
+    {
+       int a = 5;
+       int b = 6;
+     System.out.println("The product is: " + (a*b));
+
+    }
+    }
